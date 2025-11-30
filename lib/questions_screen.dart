ca10625cebc345e31a86f78f83ac824daf570b22
Thanakorn Amnuaywiboolpol <thanakorn.benjamin@gmail.com>
2025-11-30 16:10:47 +0700

    feat: Render question text and answer buttons using `questions` data.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,17 +12,20 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("This question...", style: TextStyle(color: Colors.white)),
+          Text(currentQuestion.text, style: TextStyle(color: Colors.white)),
           const SizedBox(height: 30),
-          AnswerButton(answerText: "Answer 1", onTap: () {}),
-          AnswerButton(answerText: "Answer 2", onTap: () {}),
-          AnswerButton(answerText: "Answer 3", onTap: () {}),
-          AnswerButton(answerText: "Answer 4", onTap: () {}),
+
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
         ],
       ),
     );
